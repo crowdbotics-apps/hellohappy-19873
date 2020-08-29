df8fe8a7dd8b3f5850de60e00c1866a71d08ea82
Crowdbotics <team@crowdbotics.com>
2020-08-29 21:44:20 +0000

    add package blueprint-react-native-camera-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Camera795438', human_name: 'Camera 7', access_route: 'Camera795438', icon: 'camera'},
 { name: 'CalendarView895437', human_name: 'Calendar View 8', access_route: 'CalendarView895437', icon: 'calendar'},
 { name: 'ArticleList995436', human_name: 'Article List 9', access_route: 'ArticleList995436'},
 { name: 'UserProfile1095435', human_name: 'User Profile 10', access_route: 'UserProfile1095435'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Camera795438Navigator from '../features/Camera795438/navigator';
 import CalendarView895437Navigator from '../features/CalendarView895437/navigator';
 import ArticleList995436Navigator from '../features/ArticleList995436/navigator';
 import UserProfile1095435Navigator from '../features/UserProfile1095435/navigator';
@@ -16,6 +17,7 @@ import UserProfile1095435Navigator from '../features/UserProfile1095435/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Camera795438: { screen: Camera795438Navigator },
 CalendarView895437: { screen: CalendarView895437Navigator },
 ArticleList995436: { screen: ArticleList995436Navigator },
 UserProfile1095435: { screen: UserProfile1095435Navigator },
